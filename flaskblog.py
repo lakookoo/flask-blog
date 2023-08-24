2faa92cc87fccbe4d0480d23c1cdbbafaa189e14
Larisa <kosareva.larisa@gmail.com>
2023-08-24 14:02:12 +0200

    L2.3 add navbar, article, and styles into static folder

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -24,7 +24,7 @@ def hello():
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title='About')
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
