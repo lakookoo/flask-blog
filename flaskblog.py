e80829421e883193d817850511515384f05e2785
Larisa <kosareva.larisa@gmail.com>
2023-08-28 17:41:53 +0200

    L3.7 add login page, fix register

--- flaskblog.py
+++ flaskblog.py
@@ -35,9 +35,12 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form )
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Log in {form.username.data}!', '_success_')
+        return redirect(url_for('home'))
     return render_template('login.html', title='Login', form=form )
 
 if __name__ == '__main__':
