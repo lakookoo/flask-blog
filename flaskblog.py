04ba37a29a851fe447c398a20d46553237c6a530
Larisa <kosareva.larisa@gmail.com>
2023-08-28 12:06:58 +0200

    L3.3 add register template, fix bugs

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, url_for
-
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = ''
+app.config['SECRET_KEY'] = 'fb3b44081c45e4e93bd62d6b377af670'
 posts = [
     {
         'author':'Larisa',
@@ -26,6 +26,16 @@ def hello():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form )
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form )
 
 if __name__ == '__main__':
     app.run(debug=True)
